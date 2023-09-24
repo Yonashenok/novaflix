c547a8ea60eebefcb742cd2bc5a988a590fed201
Yonashenok <yonashenok24@gmail.com>
2023-09-24 21:11:47 +0300

    feat: react router setup path

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Route, Routes, Outlet } from "react-router-dom";
+import { fetchShow } from "./redux/search/searchSlice";
+import Header from "./components/header/Header";
+import TvShow from "./pages/TvShow";
+import Movies from "./pages/Movies";
+import Favorite from "./pages/Favorite";
+import Footer from "./components/footer/footer";
+import "./App.css";
+
+function Layout() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchShow());
+  }, [dispatch]);
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
+}
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Movies />} />
+          <Route path="/tvshows" element={<TvShow />} />
+          <Route path="/favorites" element={<Favorite />} />
+          <Route path="*" element={<div>page not found </div>} />
+        </Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
